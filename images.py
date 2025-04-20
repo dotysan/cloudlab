de7473ced7bf9ac162667177a36d442c0da74f3a
Curtis Doty <Curtis@GreenKey.net>
2025-04-20 22:00:20 +0000

    don't endlessly reboot  - add ubuntu images

--- images.py
+++ images.py
@@ -1,1 +1,4 @@
+
+ubuntu20 = 'urn:publicid:IDN+emulab.net+image+emulab-ops/UBUNTU20-64-STD'
+ubuntu22 = 'urn:publicid:IDN+emulab.net+image+emulab-ops/UBUNTU22-64-STD'
 ubuntu24 = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD'
--- ubuntu-min.sh
+++ ubuntu-min.sh
@@ -2,6 +2,8 @@
 # needs to be run as root/sudo
 set -xe
 
+# TODO: detect if this has already run and skip it
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get --yes purge \
@@ -12,6 +14,7 @@ apt-get --yes purge \
     python3-botocore \
     snapd \
     ubuntu-pro-client \
+    unattended-upgrades \
     #
 apt-get --yes install \
     mc \
@@ -21,4 +24,5 @@ apt-get --yes dist-upgrade
 apt-get install --reinstall linux-firmware
 
 # since we added new kernel
-shutdown -r now
+#shutdown -r now
+# doh! this script runs on each boot
